b9f979df23c2f3a475c434240e29c0239d0e894e
premjogu007 <47113551+premjogu007@users.noreply.github.com>
2020-07-15 17:52:38 +0530

    first commit

---
+++ Navigation_page.py
@@ -0,0 +1,117 @@
+from selenium import webdriver
+import time
+import html
+import sys, os
+from datetime import datetime,timedelta
+import Global_var
+import wx
+import string
+import html
+import re
+from Scraping_things import scrap_data
+import urllib.request
+import urllib.parse
+import requests
+
+app = wx.App()
+
+def ChromeDriver():
+    browser = webdriver.Chrome(executable_path=str(f"F:\\chromedriver.exe"))
+
+    browser.get("https://www.hankintailmoitukset.fi/en/search")
+    browser.maximize_window()
+    time.sleep(5)
+    Tender_count = ''
+    for Tender_count in browser.find_elements_by_xpath('//*[@class="search-result-count p-3 px-4"]/strong'):
+        Tender_count = Tender_count.get_attribute('innerText').strip()
+        break
+    TenderDeadline_list = []
+    count = 0
+    for tr in range(1, int(Tender_count), 1):
+        for published in browser.find_elements_by_xpath(f'/html/body/div[1]/main/div/div/div[2]/div/table/tbody/tr[{str(tr)}]/td[3]/span'):
+            published = published.get_attribute('innerText').strip()
+            break
+        datetime_object = datetime.strptime(published, '%d.%m.%Y %H:%M')
+        publish_date = datetime_object.strftime("%d-%m-%Y")
+
+        datetime_object_pub = datetime.strptime(publish_date, '%d-%m-%Y')
+        User_Selected_date = datetime.strptime(str(Global_var.From_Date), '%d-%m-%Y')
+
+        timedelta_obj = datetime_object_pub - User_Selected_date
+        day = timedelta_obj.days
+        
+        if day >= 0:
+            for FI_EU_Condition in browser.find_elements_by_xpath(f'/html/body/div[1]/main/div/div/div[2]/div/table/tbody/tr[{str(tr)}]/td[1]/span'):
+                FI_EU_Condition = FI_EU_Condition.get_attribute('innerText').strip()
+                break
+            if FI_EU_Condition == 'FI':
+                Tender_link = ''
+                Tender_Deadline = ''
+                for Tender_link in browser.find_elements_by_xpath(f'/html/body/div[1]/main/div/div/div[2]/div/table/tbody/tr[{str(tr)}]/td[2]/a'):
+                    Tender_link = Tender_link.get_attribute('href').strip()
+                    break
+                for Tender_Deadline in browser.find_elements_by_xpath(f'/html/body/div[1]/main/div/div/div[2]/div/table/tbody/tr[{str(tr)}]/td[4]/span'):
+                    Tender_Deadline = Tender_Deadline.get_attribute('innerText').strip()
+                    break
+                if Tender_Deadline != '':
+                    datetime_object = datetime.strptime(Tender_Deadline, '%d.%m.%Y %H:%M')
+                    Tender_Deadline = datetime_object.strftime("%Y-%m-%d")
+                    TenderDeadline_list.append(f'{Tender_link}[]@#$%{Tender_Deadline}')
+                    count += 1
+                    print(f'Link Collect: {str(count)}')
+                else:
+                    Global_var.deadline_Not_given += 1
+                    print('Deadline Not Given')
+        else:
+            print('Publish Date Dead')
+            break
+    print(f"\nTotal Link Collected {len(TenderDeadline_list)}")
+
+    navigation_things(TenderDeadline_list,browser)
+
+def navigation_things(TenderDeadline_list,browser):
+    for href in TenderDeadline_list:  # https://www.hankintailmoitukset.fi/fi/public/procurement/38936/notice/50890/[]@#$%2020-07-24'
+        Tender_link = str(href).partition("[]@#$%")[0].strip()  # https://www.hankintailmoitukset.fi/fi/public/procurement/38936/notice/50890/
+        Tender_deadline = str(href).partition("[]@#$%")[2].strip() # 2020-07-24
+        browser.get(Tender_link)
+        time.sleep(4)
+        notice_number = ''
+        for notice_number in browser.find_elements_by_xpath('//*[@class="header-subscript flex-column"]'):
+            notice_number = notice_number.get_attribute('outerHTML').strip().replace('<!---->','').replace('-\t','').replace('-\n','').replace('\t','').replace('\n','')
+            notice_number = html.unescape(str(notice_number))
+            notice_number = re.sub(' +', ' ', str(notice_number))
+            notice_number =  notice_number.partition('Ilmoituksen numero</span>')[2].partition("</span>")[0].strip()
+            notice_number =  notice_number.partition('">')[2]
+            break
+        time.sleep(4)
+        tab_links_list = []
+        for tab_links in browser.find_elements_by_xpath('//*[@class="progress-nav categoryList nav nav-tabs pl-0"]/li/a'):
+            tab_links_list.append(tab_links.get_attribute('href'))
+            
+        get_htmlsource = ''
+        for tab_link in tab_links_list:
+            browser.get(tab_link)
+            time.sleep(4)
+            pos = [] #list to store positions for each 'char' in 'string'
+            for n in range(len(tab_link)):
+                if tab_link[n] == '/':
+                    pos.append(n)
+            tab_link_class = tab_link[pos[-1]:]
+            tab_link_class = tab_link_class.partition('/')[2].strip()
+            tab_outerhtml = ''
+            for tab_outerhtml in browser.find_elements_by_xpath(f'//*[@class="notice-public-{tab_link_class} card-body preview"]'):
+                tab_outerhtml = tab_outerhtml.get_attribute('outerHTML').strip().replace('<!---->','').replace('-\t','').replace('-\n','').replace('\t','').replace('\n','')
+                tab_outerhtml = html.unescape(str(tab_outerhtml))
+                tab_outerhtml = re.sub(' +', ' ', str(tab_outerhtml))
+                get_htmlsource += tab_outerhtml
+        RM_button =  get_htmlsource.partition('<button')[2].partition("</button>")[0].strip()
+        get_htmlsource = get_htmlsource.replace(RM_button,'').replace('<button','').replace('</button>','')
+        scrap_data(Tender_link,Tender_deadline,get_htmlsource,notice_number)
+        print(f'Total: {str(len(TenderDeadline_list))} Deadline Not given: {Global_var.deadline_Not_given} duplicate: {Global_var.duplicate} inserted: {Global_var.inserted} expired: {Global_var.expired} QC Tenders: {Global_var.QC_Tenders}')
+    
+    wx.MessageBox(f'Total: {str(len(TenderDeadline_list))}\nDeadline Not given: {Global_var.deadline_Not_given}\nduplicate: {Global_var.duplicate}\ninserted: {Global_var.inserted}\nexpired: {Global_var.expired}\nQC Tenders: {Global_var.QC_Tenders}','hankintailmoitukset.fi', wx.OK | wx.ICON_INFORMATION)
+    browser.close()
+    sys.exit() 
+
+
+ChromeDriver()
---
+++ Scraping_things.py
@@ -0,0 +1,173 @@
+import time
+from datetime import datetime
+import Global_var
+from Insert_On_Datbase import insert_in_Local
+import sys , os
+import string
+import time
+from datetime import datetime
+import html
+import re
+import wx
+app = wx.App()
+
+def remove_html_tag(string):
+    cleanr = re.compile('<.*?>')
+    main_string = re.sub(cleanr, '', string)
+    return main_string
+
+def scrap_data(Tender_link,Tender_deadline,get_htmlsource,notice_number):
+
+    SegField = []
+    for data in range(42):
+        SegField.append('')
+    
+    a = True
+    while a == True:
+        try:
+            Email = get_htmlsource.partition('Sähköpostiosoite </div>')[2].partition("</div>")[0].strip()
+            Email = remove_html_tag(Email)
+            SegField[1] = Email.strip()
+            
+            name_and_address = get_htmlsource.partition('Nimi ja osoitteet</span>')[2].strip()
+
+            buyer_name = name_and_address.partition('Virallinen nimi </div>')[2].partition("</div>")[0].strip()
+            buyer_name = remove_html_tag(buyer_name)
+
+            SegField[12] = buyer_name
+
+            Postal_address = name_and_address.partition('Postiosoite </div>')[2].partition("</div>")[0].strip()
+            Postal_address = remove_html_tag(Postal_address)
+
+            Postal_code = name_and_address.partition('Postinumero </div>')[2].partition("</div>")[0].strip()
+            Postal_code = remove_html_tag(Postal_code)
+
+            Town = name_and_address.partition('Postitoimipaikka </div>')[2].partition("</div>")[0].strip()
+            Town = remove_html_tag(Town)
+
+            country =  name_and_address.partition('Maa </div>')[2].partition("</div>")[0].strip()
+            country = remove_html_tag(country)
+
+            telphone =  get_htmlsource.partition('Puhelin </div>')[2].partition("</div>")[0].strip()
+            telphone = remove_html_tag(telphone)
+
+            Proper_address = f'{buyer_name}<br>\n{Postal_address.strip()}, {Town.strip()}, {country.strip()} - {Postal_code.strip()}, Tel: {telphone.strip()}'
+            SegField[2] = Proper_address.strip()
+
+            purchase_url = get_htmlsource.partition('Verkko-osoite')[2].partition("</a>")[0].strip()
+            purchase_url = purchase_url.partition('href="')[2].partition('"')[0].strip()
+            SegField[8] = purchase_url.strip()
+            
+            Title =  get_htmlsource.partition('Hankinnan nimi </div>')[2].partition("</div>")[0].strip()
+            Title = remove_html_tag(Title)
+            Title = string.capwords(str(Title))
+            SegField[19] = Title.strip()
+
+            Tender_detail = get_htmlsource.partition('Hankinnan lyhyt kuvaus')[2].partition('class="notice-public-standard')[0].strip()
+            Tender_detail = Tender_detail.partition('class="value-row-value')[2]
+            Tender_detail = remove_html_tag(Tender_detail)
+            Tender_detail = Tender_detail.partition('">')[2].partition('<div')[0].strip()
+            if len(Tender_detail) >= 800:
+                Tender_detail = str(Tender_detail)[:800]+'...'
+            Type_of_contract = get_htmlsource.partition('Sopimuksen tyyppi</div></div>')[2].partition('</div>')[0].strip()
+            Type_of_contract = remove_html_tag(Type_of_contract)
+
+            NUTS_code = get_htmlsource.partition('NUTS-koodi</div>')[2].partition('</span>')[0].strip()
+            NUTS_code = remove_html_tag(NUTS_code)
+
+            Type_of_procedure = get_htmlsource.partition('Menettelyn luonne </div>')[2].partition('</div>')[0].strip()
+            Type_of_procedure = remove_html_tag(Type_of_procedure)
+
+            SegField[18] = f'{SegField[19]}<br>\nSLyhyt kuvaus: {Tender_detail.strip()}<br>\nSopimuksen tyyppi: {Type_of_contract.strip()}<br>\nNUTS-koodi: {NUTS_code.strip()}<br>\nMenettelyn luonne: {Type_of_procedure.strip()}'
+
+            SegField[18] = string.capwords(str(SegField[18]))
+
+            SegField[13] = notice_number.strip()
+
+            SegField[14] = '2'
+            SegField[22] = "0"
+            SegField[26] = "0.0"
+            SegField[27] = "0"  # Financier
+            SegField[24] = Tender_deadline.strip()
+            SegField[7] = 'FI'
+            SegField[28] = Tender_link
+            SegField[31] = 'hankintailmoitukset.fi'
+            ReplyStrings = get_htmlsource.partition('CPV-koodi</div></div>')[2].partition("</div>")[0].strip()
+            ReplyStrings = remove_html_tag(ReplyStrings)
+            if ReplyStrings != "":
+                copy_cpv = ""
+                Cpv_status = True
+                all_string = ""
+                try:
+                    while Cpv_status == True:
+                        phoneNumRegex = re.compile(r'\d\d\d\d\d\d\d\d')
+                        CPv_main = phoneNumRegex.search(ReplyStrings)
+                        mainNumber = CPv_main.groups()
+                        if CPv_main:
+                            copy_cpv = CPv_main.group(), ", "
+                            ReplyStrings = ReplyStrings.replace(CPv_main.group(), "")
+                        else:
+                            Cpv_status = False
+                        result = "".join(str(x) for x in copy_cpv)
+                        result = result.replace("-", "").strip()
+                        result2 = result.replace("\n", "")
+                        # print(result2)
+                        all_string += result2.strip(",")
+                except:
+                    pass
+                print(all_string.strip(","))
+                SegField[36] = all_string
+            else:
+                SegField[36] = ""
+
+            for SegIndex in range(len(SegField)):
+                print(SegIndex, end=' ')
+                print(SegField[SegIndex])
+                SegField[SegIndex] = html.unescape(str(SegField[SegIndex]))
+                SegField[SegIndex] = str(SegField[SegIndex]).replace("'", "''")
+
+            if len(SegField[19]) >= 200:
+                SegField[19] = str(SegField[19])[:200]+'...'
+
+            if len(SegField[18]) >= 1500:
+                SegField[18] = str(SegField[18])[:1500]+'...'
+
+            if SegField[19] == '':
+                wx.MessageBox(' Short Desc Blank ','hankintailmoitukset.fi', wx.OK | wx.ICON_INFORMATION)
+            else:
+                check_date(get_htmlsource, SegField)
+                pass
+
+            a = False
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print("Error ON : ", sys._getframe().f_code.co_name + "--> " + str(e), "\n", exc_type, "\n", fname, "\n",
+                  exc_tb.tb_lineno)
+            a = True
+            time.sleep(5)
+
+
+def check_date(get_htmlSource, SegField):
+    deadline = str(SegField[24])
+    curdate = datetime.now()
+    curdate_str = curdate.strftime("%Y-%m-%d")
+    try:
+        if deadline != '':
+            datetime_object_deadline = datetime.strptime(deadline, '%Y-%m-%d')
+            datetime_object_curdate = datetime.strptime(curdate_str, '%Y-%m-%d')
+            timedelta_obj = datetime_object_deadline - datetime_object_curdate
+            day = timedelta_obj.days
+            if day > 0:
+                insert_in_Local(get_htmlSource, SegField)
+                print('Live Tender')
+            else:
+                print("Expired Tender")
+                Global_var.expired += 1
+        else:
+            print("Deadline Not Given")
+            Global_var.deadline_Not_given += 1
+    except Exception as e:
+        exc_type , exc_obj , exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("Error ON : " , sys._getframe().f_code.co_name + "--> " + str(e) , "\n" , exc_type , "\n" , fname , "\n" ,exc_tb.tb_lineno)
